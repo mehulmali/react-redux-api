851140ed60d9bd88c7b3063f0eaa0d03ed27b195
sweety-wo <sweety.webashlar@gmail.com>
2016-10-20 19:03:39 +0530

    logout

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -4,20 +4,57 @@
 
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { logout } from '../actions/authActions';
 
-export default () => {
-    return (
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link to="/" className="navbar-brand">React-Redux App</Link>
+class NavigationBar extends React.Component {
+    logout(e){
+        e.preventDefault();
+        this.props.logout();
+    }
+
+    render() {
+        const { isAuthenticated } = this.props.auth;
+
+        const userLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li><a href="#" onClick={this.logout.bind(this)}><span className="glyphicon glyphicon-log-out"></span> Logout</a></li>
+            </ul>
+        );
+
+        const guestLinks = (
+            <ul className="nav navbar-nav navbar-right">
+                <li><Link to="/signup"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
+                <li><Link to="/login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
+            </ul>
+        );
+
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <Link to="/" className="navbar-brand">React-Redux App</Link>
+                    </div>
+
+
+                    <div className="collapse navbar-collapse">
+                        { isAuthenticated ? userLinks : guestLinks }
+                    </div>
                 </div>
-
-                <ul className="nav navbar-nav navbar-right">
-                    <li><Link to="/signup"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
-                    <li><Link to="/login"><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
-                </ul>
-            </div>
-        </nav>
-    )
+            </nav>
+        )
+    }
 }
+
+NavigationBar.protoTypes = {
+    auth: React.PropTypes.object.isRequired,
+    logout: React.PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    };
+}
+
+export default connect(mapStateToProps, {logout})(NavigationBar);
